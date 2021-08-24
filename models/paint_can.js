3befc44ef6aa1713cc49abc24273331ed4363f71
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-24 16:07:16 +0100

    check paintcan empty - prototype and tests written and passing

--- models/paint_can.js
+++ models/paint_can.js
@@ -2,4 +2,16 @@
 const PaintCan = function (volume) {
     this.volume = volume
 }
+
+PaintCan.prototype.checkIfEmpty = function (){
+    if (this.volume === 0) {
+        return "This can is empty"
+    } else {
+        return "You have paint... get to it"
+    }
+}
+
+
+
+
 module.exports = PaintCan;
--- models/specs/paint_can_spec.js
+++ models/specs/paint_can_spec.js
@@ -2,18 +2,22 @@ const assert = require('assert');
 const PaintCan = require('../paint_can.js');
 
 describe('Paint Can', function () {
-
+  let paintCan;
 
   beforeEach(function () {
-   
+    paintCan = new PaintCan (10)
   });
 
-  xit('should have a number of litres of paint', function () {
-    
+  it('should have a number of litres of paint', function () {
+    const actual = paintCan.volume
+    assert.strictEqual(actual, 10)
   });
 
-  xit('should not start empty', function () {
+  it('should not start empty', function () {
     
+    const actual = paintCan.checkIfEmpty();
+    assert.strictEqual(actual, "You have paint... get to it")
+
   });
 
   xit('should be able to empty itself', function () {
